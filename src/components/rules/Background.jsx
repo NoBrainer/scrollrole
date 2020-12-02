ea92d4f7b48e73aacfec686e03791d0286f4d9e3
Vincent Incarvite <vincent.incarvite@gmail.com>
2020-12-02 09:52:56 -0500

    Display background features

--- src/components/rules/Background.jsx
+++ src/components/rules/Background.jsx
@@ -3,27 +3,33 @@ import {makeStyles} from "@material-ui/core/styles";
 import {ExpandMore} from "@material-ui/icons";
 import {PAPER_STYLES} from "common/Defaults";
 import Description from "components/rules/Description";
+import Features from "components/rules/Features";
 import PropTypes from "prop-types";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
-	accordion: {},
+	details: {
+		flexDirection: "column",
+	},
 	paper: PAPER_STYLES,
 }));
 
 function Background(props) {
 	const classes = useStyles();
 	const {background, summaryId} = props;
-	const {description, name} = background;
+	const {description, features, name} = background;
 
 	//TODO: populate the entire structure
+	console.log(background);
+
 	return (
-		<Accordion className={classes.accordion}>
+		<Accordion>
 			<AccordionSummary expandIcon={<ExpandMore/>} aria-controls="background-panel-content" id={summaryId}>
 				<Typography component="h2" variant="h5">{name}</Typography>
 			</AccordionSummary>
-			<AccordionDetails>
+			<AccordionDetails className={classes.details}>
 				<Description description={description}/>
+				<Features features={features}/>
 			</AccordionDetails>
 		</Accordion>
 	);
---
+++ src/components/rules/Features.jsx
@@ -0,0 +1,42 @@
+import {Typography} from "@material-ui/core";
+import RulesSection from "components/rules/RulesSection";
+import {isEmpty} from "lodash";
+import PropTypes from "prop-types";
+import React from "react";
+
+function Features(props) {
+	const {features} = props;
+
+	if (isEmpty(features)) return null;
+
+	const renderParagraphs = (description) => {
+		return description.map((paragraph, i) => {
+			return (<Typography key={i} paragraph={true}>{paragraph}</Typography>);
+		});
+	}
+
+	const renderFeatures = () => {
+		return features.map((feature, i) => {
+			return (<RulesSection key={i} headerText={feature.name} children={renderParagraphs(feature.description)}/>);
+		});
+	};
+
+	return (
+		<RulesSection headerText="Features" children={renderFeatures()}/>
+	);
+
+	// TODO: replace above with this
+	// const renderFeatures = () => {
+	// 	return features.map((feature, i) => {
+	// 		return (<RulesSection key={i} headerText={`Feature: ${feature.name}`} children={renderParagraphs(feature.description)}/>);
+	// 	});
+	// };
+	//
+	// return renderFeatures();
+}
+
+Features.propTypes = {
+	features: PropTypes.array,
+};
+
+export default Features
