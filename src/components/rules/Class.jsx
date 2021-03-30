72cf5a5a169ee7371359382ae47f1a1461eb4a8c
NoBrainer <vinniemac189@gmail.com>
2021-03-30 09:40:37 -0400

    Added some of the Class fields

--- src/components/rules/Class.jsx
+++ src/components/rules/Class.jsx
@@ -1,6 +1,11 @@
 import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import {ExpandMore} from "@material-ui/icons";
+import Choices from "components/rules/Choices";
+import Description from "components/rules/Description";
+import Equipment from "components/rules/Equipment";
+import Features from "components/rules/Features";
+import Proficiencies from "components/rules/Proficiencies";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -13,7 +18,10 @@ const useStyles = makeStyles((theme) => ({
 function Class(props) {
 	const classes = useStyles();
 	const {clazz, summaryId} = props;
-	const {name} = clazz; //TODO: implement other fields
+	const {
+		name, hitDice, baseHitPoints, proficiencies, equipment, proficiencyBonus, spellCasting,
+		abilityScoreImprovementLevels, features, choices, unlockables
+	} = clazz;
 
 	return (
 		<Accordion>
@@ -21,7 +29,15 @@ function Class(props) {
 				<Typography component="h2" variant="h2">{name}</Typography>
 			</AccordionSummary>
 			<AccordionDetails className={classes.details}>
-				<Typography>[UNDER CONSTRUCTION]</Typography>
+				<Description headerText={"Hit Dice"} description={[hitDice]}/>
+				<Description headerText={"Base Hit Points"} description={[baseHitPoints]}/>
+				<Proficiencies proficiencies={proficiencies}/>
+				<Equipment equipment={equipment}/>
+				<Description headerText={"Proficiency Bonus"} description={[proficiencyBonus]}/>
+				<Description headerText={"Ability Score Improvement Levels"} description={["[UNDER CONSTRUCTION]"]}/>
+				<Features features={features}/>
+				<Choices choices={choices}/>
+				<Description headerText={"Unlockables"} description={["[UNDER CONSTRUCTION]"]}/>
 			</AccordionDetails>
 		</Accordion>
 	);
