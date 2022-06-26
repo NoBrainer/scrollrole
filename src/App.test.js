d3b5e92e4dab51f152e3a78fc122ff51d1791dea
NoBrainer <vinniemac189@gmail.com>
2022-06-26 01:22:58 -0400

    Deleted App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders logo', () => {
-  render(<App />);
-  const logo = screen.getByAltText(/ScrollRole logo/i);
-  expect(logo).toBeInTheDocument();
-});
