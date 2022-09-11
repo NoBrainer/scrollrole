392629b3512c24353dbd29e270f09188268d6633
NoBrainer <vinniemac189@gmail.com>
2022-09-11 19:41:25 -0400

    WIP: Improving the nav tabs

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,78 +1,33 @@
 import { People as PeopleIcon, Settings as SettingsIcon } from '@mui/icons-material';
 import { AppBar, Tab, Tabs, Toolbar } from '@mui/material';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from 'assets/logo.svg';
 import { APP_BAR_HEIGHT } from 'common/constants';
-import { usePageState } from 'common/pageState';
+import { buildUrl, usePageState } from 'common/pageState';
 import PagePadder from 'components/scaffolding/PagePadder';
 
-function TabIconLink({ ariaLabel, url, value, children } = {}) {
-  return (
-    <Tab
-      aria-label={ariaLabel}
-      sx={{ mr: 2, opacity: 1, padding: 0 }}
-      value={value}
-      icon={
-        <Link
-          to={url}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            minHeight: `${APP_BAR_HEIGHT}px`,
-            minWidth: `${APP_BAR_HEIGHT}px`,
-          }}
-        >
-          {children}
-        </Link>
-      }
-      // icon={<>{children}</>}
-    />
-  );
-}
-
-TabIconLink.propTypes = {
-  ariaLabel: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-  url: PropTypes.string.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-const TAB_VALUE_MAP = {
-  characters: 1,
-  rules: 2,
-};
-
+// TODO: Get better icons from game-icons.net
 function AppHeader() {
   const [pageState] = usePageState();
+  const navigate = useNavigate();
 
-  const onTabChange = (e) => {
-    console.log(`~ onTabChange(%o)`, e);
-    // TODO: figure out if we want to use this handler or just do things with Links
-  };
+  const onTabChange = useCallback((e, newValue) => {
+    navigate(buildUrl(newValue));
+  }, []);
 
   return (
     <AppBar position='relative' sx={{ height: `${APP_BAR_HEIGHT}px` }}>
       <Toolbar>
         <PagePadder>
-          <Tabs value={TAB_VALUE_MAP[pageState.pageId] || 0} onChange={onTabChange}>
+          <Tabs value={pageState.pageId || 'home'} onChange={onTabChange}>
             <Tab
               aria-label='home'
-              value={0}
-              icon={
-                <Link to='/'>
-                  <img src={logo} alt='ScrollRole logo' style={{ height: `${APP_BAR_HEIGHT}px` }} />
-                </Link>
-              }
+              value='home'
+              icon={<img src={logo} alt='ScrollRole logo' style={{ height: `${APP_BAR_HEIGHT}px` }} />}
             />
-            <TabIconLink ariaLabel='character builder' url='/characters' value={1}>
-              <PeopleIcon />
-            </TabIconLink>
-            <TabIconLink ariaLabel='rules customizer' url='/rules' value={2}>
-              <SettingsIcon />
-            </TabIconLink>
+            <Tab aria-label='character builder' value='characters' icon={<PeopleIcon />} />
+            <Tab aria-label='rules customizer' value='rules' icon={<SettingsIcon />} />
           </Tabs>
         </PagePadder>
       </Toolbar>
