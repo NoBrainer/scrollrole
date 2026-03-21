51a1833f15a5679e8f97ba348212cb01fb741db6
NoBrainer <vinniemac189@gmail.com>
2026-03-21 18:08:45 -0400

    Implemented RulesChoicesCard

--- lib/presentation/screen/rules/details/common/rules_choices_card.dart
+++ lib/presentation/screen/rules/details/common/rules_choices_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scrollrole/data/model/rules/query/choice.dart';
 import 'package:scrollrole/presentation/common/basic_card.dart';
+import 'package:scrollrole/presentation/screen/rules/details/common/rules_paragraphs.dart';
 
 class RulesChoicesCard extends StatelessWidget {
   final List<Choice> choices;
@@ -13,11 +14,48 @@ class RulesChoicesCard extends StatelessWidget {
       return SizedBox.shrink();
     }
 
-    // TODO: Finish RulesChoicesCard
+    List<Widget> choiceItems = choices
+        .map((c) => _ChoiceItem(choice: c))
+        .toList();
+    List<Widget> widgets = [];
+    for (Widget choiceItem in choiceItems) {
+      if (widgets.isNotEmpty) {
+        // Add space between each choice
+        widgets.add(SizedBox(height: 16));
+      }
+      widgets.add(choiceItem);
+    }
+
     return BasicCard(
       children: [
         BasicCardTitle(text: 'Choices'),
-        BasicCardSection(children: [Text('TBD')]),
+        BasicCardSection(children: widgets),
+      ],
+    );
+  }
+}
+
+class _ChoiceItem extends StatelessWidget {
+  final Choice choice;
+
+  const _ChoiceItem({required this.choice});
+
+  @override
+  Widget build(BuildContext context) {
+    String name = choice.name;
+    List<String> description = choice.description;
+    int pick = choice.pick;
+    bool allowDuplicate = choice.allowDuplicate;
+
+    final titleStyle = Theme.of(context).textTheme.bodyLarge?.copyWith();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(name, style: titleStyle),
+        RulesParagraphs(paragraphs: description),
+        SizedBox(height: 8),
+        Text('Pick $pick ${allowDuplicate ? '(duplicates allowed)' : ''}'),
       ],
     );
   }
